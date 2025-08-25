b18b11d5554dad7c4937c526b4adbe6c821032f2
Victor <vidrums86@gmail.com>
2025-08-25 23:53:47 +0100

    upd: set headers, allowed cors options

--- api/contacts.js
+++ api/contacts.js
@@ -1,3 +1,15 @@
+const cors = require("cors");
+
+const ALLOWED_ORIGINS = ["http://localhost:3001", "https://vt-portfolio.info"];
+
 export default async function (req, res) {
-  return res.status(200).json({ message: "Hello from server" });
+  const origin = req.headers.origin || "";
+  const isAllowed = ALLOWED_ORIGINS.includes(origin);
+
+  if (isAllowed) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  const body = req.body;
+
+  return res.status(200).json({ message: "Hello from server", body });
 }
