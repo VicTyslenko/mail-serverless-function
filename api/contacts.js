4bc1b15d4d457cd6f2555fd572219d807c114446
Victor <vidrums86@gmail.com>
2025-08-26 22:19:00 +0100

    feat: added nodemailer

--- api/contacts.js
+++ api/contacts.js
@@ -1,6 +1,10 @@
+import nodemailer from "nodemailer";
+
 const ALLOWED_ORIGINS = ["http://localhost:3001", "https://vt-portfolio.info"];
 
 export default async function handler(req, res) {
+  const { email, name, message } = req.body;
+
   const origin = req.headers.origin || "";
   const isAllowed = ALLOWED_ORIGINS.includes(origin);
 
@@ -15,10 +19,36 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  // Actual request
-  const body = req.body;
-  return res.status(200).json({
-    message: "Hello from server",
-    body,
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      //Gmail service will check the 'from' value is equal to auth.user in transporter to
+      from: `Victor ${process.env.USER_EMAIL}`,
+      to: process.env.RECEIVER_EMAIL,
+      replyTo: email,
+      subject: "New contact message",
+      text: `
+            You have received a new message from the contact form:
+            Name: ${name}
+            Email: ${email}
+            Message: ${message}
+          `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return res.status(200).json({ success: true, message: "Email sent" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res.status(500).json({ success: false, message: "Error sending email" });
+  }
 }
