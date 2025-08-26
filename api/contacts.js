31633c0f7fe84e5eb9f0a7f66f35abeb00f4abf0
Victor <vidrums86@gmail.com>
2025-08-26 23:13:16 +0100

    test again...

--- api/contacts.js
+++ api/contacts.js
@@ -7,19 +7,19 @@ export default async function handler(req, res) {
 
   const origin = req.headers.origin || "";
   const isAllowed = ALLOWED_ORIGINS.includes(origin);
-  
+
   if (isAllowed) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
+    // res.setHeader("Vary", "Origin");
   }
 
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 
-  // Preflight
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
+  // // Preflight
+  // if (req.method === "OPTIONS") {
+  //   return res.status(200).end();
+  // }
 
   // if (req.method !== "POST") {
   //   return res.status(405).json({ success: false, message: "Method not allowed" });
@@ -57,5 +57,5 @@ export default async function handler(req, res) {
   //   console.error("Error sending email:", error);
   //   return res.status(500).json({ success: false, message: "Error sending email" });
   // }
-  return res.status(200).json({ message: "Hello from server", email, name });
+  return res.status(200).json({ message: "Hello from server" });
 }
