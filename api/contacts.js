617cacf4aedb97f2128f6a889fa00cc7b00be417
Victor <vidrums86@gmail.com>
2025-08-26 22:31:35 +0100

    testing...

--- api/contacts.js
+++ api/contacts.js
@@ -8,21 +8,21 @@ export default async function handler(req, res) {
   const origin = req.headers.origin || "";
   const isAllowed = ALLOWED_ORIGINS.includes(origin);
 
-  res.setHeader("Access-Control-Allow-Origin", origin);
-  res.setHeader("Vary", "Origin");
+  // res.setHeader("Access-Control-Allow-Origin", origin);
+  // res.setHeader("Vary", "Origin");
 
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  // res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 
   // Preflight
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
+  // if (req.method === "OPTIONS") {
+  //   return res.status(200).end();
+  // }
 
   // if (req.method !== "POST") {
   //   return res.status(405).json({ success: false, message: "Method not allowed" });
   // }
-  
+
   return res.status(200).json({ success: true, message: "Email sent", origin });
   // try {
   //   const transporter = nodemailer.createTransport({
