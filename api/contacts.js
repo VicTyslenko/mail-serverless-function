2f4f3b068b622a86afceccb8e1a1147be61347aa
Victor <vidrums86@gmail.com>
2025-08-26 23:07:53 +0100

    options added...

--- api/contacts.js
+++ api/contacts.js
@@ -15,9 +15,9 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 
   // Preflight
-  // if (req.method === "OPTIONS") {
-  //   return res.status(200).end();
-  // }
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   // if (req.method !== "POST") {
   //   return res.status(405).json({ success: false, message: "Method not allowed" });
