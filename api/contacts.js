ac2a08ea0b825d5fcf30a13dcf3025a69779356c
Victor <vidrums86@gmail.com>
2025-08-26 22:58:40 +0100

    nodemailer

--- api/contacts.js
+++ api/contacts.js
@@ -23,37 +23,36 @@ export default async function handler(req, res) {
   //   return res.status(405).json({ success: false, message: "Method not allowed" });
   // }
 
-  return res.status(200).json({ success: true, message: "Email sent", origin });
-  // try {
-  //   const transporter = nodemailer.createTransport({
-  //     host: "smtp.gmail.com",
-  //     port: 587,
-  //     secure: false,
-  //     auth: {
-  //       user: process.env.USER_EMAIL,
-  //       pass: process.env.USER_PASSWORD,
-  //     },
-  //   });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_PASSWORD,
+      },
+    });
 
-  //   const mailOptions = {
-  //     //Gmail service will check the 'from' value is equal to auth.user in transporter to
-  //     from: `Victor ${process.env.USER_EMAIL}`,
-  //     to: process.env.RECEIVER_EMAIL,
-  //     replyTo: email,
-  //     subject: "New contact message",
-  //     text: `
-  //           You have received a new message from the contact form:
-  //           Name: ${name}
-  //           Email: ${email}
-  //           Message: ${message}
-  //         `,
-  //   };
+    const mailOptions = {
+      //Gmail service will check the 'from' value is equal to auth.user in transporter to
+      from: `Victor ${process.env.USER_EMAIL}`,
+      to: process.env.RECEIVER_EMAIL,
+      replyTo: email,
+      subject: "New contact message",
+      text: `
+            You have received a new message from the contact form:
+            Name: ${name}
+            Email: ${email}
+            Message: ${message}
+          `,
+    };
 
-  //   await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 
-  //   return res.status(200).json({ success: true, message: "Email sent", origin });
-  // } catch (error) {
-  //   console.error("Error sending email:", error);
-  //   return res.status(500).json({ success: false, message: "Error sending email" });
-  // }
+    return res.status(200).json({ success: true, message: "Email sent", origin });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res.status(500).json({ success: false, message: "Error sending email" });
+  }
 }
