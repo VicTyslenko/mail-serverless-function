6f541435ccea061f6178adc5d305bf6db1e02145
Victor <vidrums86@gmail.com>
2025-08-28 00:14:10 +0100

    7

--- api/contacts.js
+++ api/contacts.js
@@ -18,31 +18,31 @@ export default async function handler(req, res) {
 
   if (req.method === "OPTIONS") return res.status(200).end();
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.USER_EMAIL,
-      pass: process.env.USER_PASSWORD,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 587,
+  //   secure: false,
+  //   auth: {
+  //     user: process.env.USER_EMAIL,
+  //     pass: process.env.USER_PASSWORD,
+  //   },
+  // });
 
-  const mailOptions = {
-    //Gmail service will check the 'from' value is equal to auth.user in transporter to
-    from: `Victor ${process.env.USER_EMAIL}`,
-    to: process.env.RECEIVER_EMAIL,
-    replyTo: email,
-    subject: "New contact message",
-    text: `
-            You have received a new message from the contact form:
-            Name: ${name}
-            Email: ${email}
-            Message: ${message}
-          `,
-  };
+  // const mailOptions = {
+  //   //Gmail service will check the 'from' value is equal to auth.user in transporter to
+  //   from: `Victor ${process.env.USER_EMAIL}`,
+  //   to: process.env.RECEIVER_EMAIL,
+  //   replyTo: email,
+  //   subject: "New contact message",
+  //   text: `
+  //           You have received a new message from the contact form:
+  //           Name: ${name}
+  //           Email: ${email}
+  //           Message: ${message}
+  //         `,
+  // };
 
-  await transporter.sendMail(mailOptions);
+  // await transporter.sendMail(mailOptions);
 
-  return res.status(200).json({ success: true, message: "Email sent", origin });
+  return res.status(200).json({ success: true, message: "Email sent" });
 }
