322bc0a465eecb47909621d14624669e7b31f3b6
Victor <vidrums86@gmail.com>
2025-08-28 00:23:00 +0100

    14

--- api/contacts.js
+++ api/contacts.js
@@ -43,6 +43,6 @@ export default async function handler(req, res) {
     await transporter.sendMail(mailOptions);
     return res.status(200).json({ success: true, message: "Email sent" });
   } catch (error) {
-    return res.status(404).json({ message: "Fuck off!" });
+    return res.status(404).json({ error });
   }
 }
