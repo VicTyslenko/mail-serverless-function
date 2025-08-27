0c2678e642393e0a3e239f726978d0296d9520c6
Victor <vidrums86@gmail.com>
2025-08-28 00:14:57 +0100

    8

--- api/contacts.js
+++ api/contacts.js
@@ -10,11 +10,9 @@ export default async function handler(req, res) {
 
   const isAllowed = ALLOWED_ORIGINS.includes(origin);
 
-  if (isAllowed) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  }
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") return res.status(200).end();
 
