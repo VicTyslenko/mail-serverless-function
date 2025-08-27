44dd5beda0e3ce91597127b2052d5935d8ae3d85
Victor <vidrums86@gmail.com>
2025-08-27 23:29:07 +0100

    allowed all origins requests to server for testing

--- api/contacts.js
+++ api/contacts.js
@@ -7,7 +7,11 @@ export default async function handler(req, res) {
 
   const origin = req.headers.origin || "";
   // const isAllowed = ALLOWED_ORIGINS.includes(origin);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
   // if (isAllowed) {
   //   res.setHeader("Access-Control-Allow-Origin", origin);
   //   // res.setHeader("Vary", "Origin");
