69f1bbb1349e5bbe68f16c9c777ca02cc77abc8d
Victor <vidrums86@gmail.com>
2025-08-27 23:30:30 +0100

    removed preflight for Options, testing...

--- api/contacts.js
+++ api/contacts.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") return res.status(200).end();
+  // if (req.method === "OPTIONS") return res.status(200).end();
   // if (isAllowed) {
   //   res.setHeader("Access-Control-Allow-Origin", origin);
   //   // res.setHeader("Vary", "Origin");
