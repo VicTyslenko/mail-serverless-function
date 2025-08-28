a02ea2142033f0908ee5b0cde26b9e7bee65688b
Victor <vidrums86@gmail.com>
2025-08-28 20:11:30 +0100

    testing...

--- api/contacts.js
+++ api/contacts.js
@@ -15,34 +15,36 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") return res.status(200).end();
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.USER_PASSWORD,
-      },
-    });
 
-    const mailOptions = {
-      //Gmail service will check the 'from' value is equal to auth.user in transporter to
-      from: `Victor ${process.env.USER_EMAIL}`,
-      to: process.env.RECEIVER_EMAIL,
-      replyTo: email,
-      subject: "New contact message",
-      text: `
-              You have received a new message from the contact form:
-              Name: ${name}
-              Email: ${email}
-              Message: ${message}
-            `,
-    };
+  // try {
+  //   const transporter = nodemailer.createTransport({
+  //     host: "smtp.gmail.com",
+  //     port: 587,
+  //     secure: false,
+  //     auth: {
+  //       user: process.env.USER_EMAIL,
+  //       pass: process.env.USER_PASSWORD,
+  //     },
+  //   });
 
-    await transporter.sendMail(mailOptions);
-    return res.status(200).json({ success: true, message: "Email sent" });
-  } catch (error) {
-    return res.status(404).json({ errorMessage: error });
-  }
+  //   const mailOptions = {
+  //     //Gmail service will check the 'from' value is equal to auth.user in transporter to
+  //     from: `Victor ${process.env.USER_EMAIL}`,
+  //     to: process.env.RECEIVER_EMAIL,
+  //     replyTo: email,
+  //     subject: "New contact message",
+  //     text: `
+  //             You have received a new message from the contact form:
+  //             Name: ${name}
+  //             Email: ${email}
+  //             Message: ${message}
+  //           `,
+  //   };
+
+  //   await transporter.sendMail(mailOptions);
+  //   return res.status(200).json({ success: true, message: "Email sent" });
+  // } catch (error) {
+  //   return res.status(404).json({ errorMessage: error });
+  // }
+  return res.status(200).json({ message: "All good" });
 }
